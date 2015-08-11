dc359ba88116d109c7ae2c94008720d7f1b01e83
Hackbright Student <info@hackbrightacademy.com>
2015-08-11 16:43:35 -0700

    nice stuff today

---
+++ static/businesses.js
@@ -0,0 +1,94 @@
+function initialize() {
+
+    // Import mapstyles from external .js file
+    // Create a new StyledMapType object, passing it the array of styles,
+    // as well as the name to be displayed on the map type control.
+    var styledMap = new google.maps.StyledMapType(
+        MAPSTYLES,
+        {name: "Custom Style"}
+    );
+
+    // Create a map object, and include the MapTypeId to add
+    // to the map type control.
+    var mapOptions = {
+        zoom: 13,
+        center: new google.maps.LatLng(37.77018, -122.44259),
+        zoomControl: true,
+        panControl: false,
+        streetViewControl: false,
+        mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+        }
+    };
+
+    // Identify div in which to display map
+    var map = new google.maps.Map(
+        document.getElementById('single-map'),
+        mapOptions);
+
+    // Associate the styled map with the MapTypeId and set it to display.
+    map.mapTypes.set('map_style', styledMap);
+    map.setMapTypeId('map_style');
+
+    // Define global infoWindow
+    // If you do this inside the loop, the windows do
+    // not automatically close when a new marker is clicked
+    var infoWindow = new google.maps.InfoWindow({
+        width: 100
+    });
+
+
+
+
+
+
+    // Retrieving the information with AJAX
+
+        $.get('/business_list.json', function (businesses) {
+            // Attach markers to each business location in returned JSON
+            var business, marker, contentString;
+
+            for (var yelp_id in businesses) {
+                business = businesses[yelp_id];
+
+                // Define the marker
+                marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(business.bus_lat, business.bus_long),
+                    map: map,
+                    title: 'Business name: ' + business.business_name,
+                    icon: '/static/img/nails-small.png'
+                });
+
+
+                // Define the content of the infoWindow
+                contentString = (
+                    '<div class="window-content">' +
+                        '<p><b><a href=\"/salons/' + business.yelp_id + '\">' +
+                        business.business_name + '</b></a></p>' +
+                        '<p><b>Address: </b>' + business.address + '</p>' +
+                        '<p><b>Phone: </b>' + business.phone + '</p>' +
+                        '<p><b>Today\'s Hours: </b>' + business.todaysHours + '</p>' +
+                        
+                    '</div>');
+
+                // Inside the loop we call bindInfoWindow passing it the marker,
+                // map, infoWindow and contentString
+                bindInfoWindow(marker, map, infoWindow, contentString);
+            }
+
+        });
+
+    // This function is outside the for loop.
+    // When a marker is clicked it closes any currently open infowindows
+    // Sets the content for the new marker with the content passed through
+    // then it open the infoWindow with the new content on the marker that's clicked
+    function bindInfoWindow(marker, map, infoWindow, html) {
+        google.maps.event.addListener(marker, 'click', function () {
+            infoWindow.close();
+            infoWindow.setContent(html);
+            infoWindow.open(map, marker);
+        });
+    }
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
---
+++ testyelp.py
@@ -0,0 +1,179 @@
+import os
+import yelp
+import csv
+import phonenumbers
+import time
+from flask import Flask, render_template, jsonify, redirect, flash, session
+import jinja2
+import datetime
+
+app = Flask(__name__)
+app.secret_key = "yoursecretkeyhere"
+
+
+yelp_api = yelp.Api(
+    consumer_key=os.environ['YELP_CONSUMER_KEY'],
+    consumer_secret=os.environ['YELP_CONSUMER_SECRET'],
+    access_token_key=os.environ['YELP_ACCESS_TOKEN'],
+    access_token_secret=os.environ['YELP_ACCESS_TOKEN_SECRET'])
+
+
+def address_formatted(raw_address):
+    address_string = ""
+    for line in raw_address:
+        address_string = address_string + line + ' '
+    return address_string
+
+
+def phone_formatted(raw_phone):
+    phone_string = ""
+    if raw_phone:
+        phone_string = phonenumbers.parse(raw_phone, "US")
+        phone_string = phonenumbers.format_number(phone_string, phonenumbers.PhoneNumberFormat.NATIONAL)
+    return phone_string
+
+
+def yelp_to_salon_list_SF(search_term):
+    """Use this to get info about all businesses for a search term"""
+    search_results = yelp_api.Search(term=search_term, location="San Francisco, CA", radius_filter=6000, categories="beautysvc,othersalons")
+    total_results = search_results.total
+
+    offset = total_results % 20 - 1
+
+    yelp_ids_dict = {}
+
+    while offset < total_results:
+        search_results_expanded = yelp_api.Search(term=search_term, offset=offset, location="San Francisco, CA", radius_filter=6000, categories="beautysvc,othersalons")
+        for business in search_results_expanded.businesses:
+            if business.name not in yelp_ids_dict:
+                yelp_ids_dict[business.name] = { \
+                'yelp_id': business.id, \
+                'bus_lat': business.location.coordinate[u'latitude'], \
+                'bus_long': business.location.coordinate[u'longitude'], \
+                'address': address_formatted(business.location.address), \
+                'phone': phone_formatted(business.phone)}
+                print yelp_ids_dict[business.name]['phone']
+        offset = offset + 20
+
+    return yelp_ids_dict
+
+
+def read_csv():
+    """Reads manually curated .csv file of nail salons in San Francisco
+    and returns dictionary with key = yelp id, values for day of the week"""
+    yelp_ids_hours_file = open('yelp_ids10.csv')
+    yelp_ids_hours = csv.reader(yelp_ids_hours_file)
+
+    yelp_ids = {}
+
+    for row in yelp_ids_hours:
+        infos = row[0].split('|')
+        yelp_ids[infos[0]] = { \
+        'Monday': infos[1], \
+        'Tuesday': infos[2], \
+        'Wednesday': infos[3], \
+        'Thursday': infos[4], \
+        'Friday': infos[5], \
+        'saturday': infos[6], \
+        'Sunday': infos[7] }
+
+        # print infos[0], yelp_ids[infos[0]]['Monday']
+
+    return yelp_ids
+
+
+def yelp_id_search(yelp_id):
+    """Takes a yelp id and returns a dictionary with its 
+    name, latitude, longitude, addres, and phone"""
+
+    business_info = {}
+
+    business = yelp_api.GetBusiness(yelp_id)
+    business_info[business.id] = { \
+    'yelp_id': business.id.encode('ascii','ignore'), \
+    'business_name': business.name.encode('ascii','ignore'), \
+    'bus_lat': business.location.coordinate[u'latitude'], \
+    'bus_long': business.location.coordinate[u'longitude'], \
+    'address': address_formatted(business.location.address), \
+    'phone': phone_formatted(business.phone)}
+
+    # print business_info
+    return business_info
+
+
+@app.route('/')
+def map():
+    """Show map of businesses."""
+
+    return render_template("map.html")
+
+
+# @app.route('/businesses.json')
+# def business_info():
+#     """JSON information about businessess."""
+
+#     businesses = read_csv()
+#     for business in businesses:
+#         print business
+
+#     business1 = yelp_id_search('accent-on-beauty-san-francisco')
+
+
+#     return jsonify(business1)
+
+
+@app.route('/business_list.json')
+def business_list():
+    """JSON information about businessess.
+    had to remove: la-crEme-spa-san-francisco-2, remEde-spa-san-francisco-2
+    """
+    now_datetime = datetime.datetime.now()
+
+    today = now_datetime.strftime('%A')
+
+    businesses = read_csv()
+    businesses_for_map = {}
+
+    for key, values in businesses.items():
+        # time.sleep(.1)
+        # print key
+        business_infos = yelp_id_search(key)
+        if business_infos:
+            businesses_for_map.update(business_infos)
+            todays_hours = {'todaysHours': values[today]}
+            business_infos[key].update(todays_hours)
+            print business_infos
+
+
+    # print businesses_for_map
+    return jsonify(businesses_for_map)
+
+
+
+@app.route("/salons")
+def list_salonss():
+    """Return page showing all the nail salons"""
+    
+    return render_template("salons_list.html")
+
+
+@app.route("/salons/<string:id>")
+def show_melon(id):
+    """Return page showing the details of a given salon"""
+    
+    # melon = model.Melon.get_by_id(id)
+    # print melon
+    # return render_template("melon_details.html",
+    #                        display_melon=melon)
+
+    
+    return render_template("salon_info.html")
+
+
+
+
+if __name__ == "__main__":
+    app.config['DEBUG'] = True
+    # connect_to_db(app)
+    app.run()
+    print 
